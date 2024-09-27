ebefbd73d4b22ab8472c6f73be5c529b39870d97
JOSEE <123747604+ThanhNhan03@users.noreply.github.com>
2024-09-27 16:39:33 +0700

    Update MappingProfile.cs

--- WeLearnAPI/WeLearnAPI/Helpers/MappingProfile.cs
+++ WeLearnAPI/WeLearnAPI/Helpers/MappingProfile.cs
@@ -44,7 +44,6 @@ public class MappingProfile : Profile
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
             .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
             .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
-            .ForMember(c => c.FullName, opt => MapFrom(x => string Join(" ", x.FirstName, x.LastName)))
             .ForMember(dest => dest.Birthday, opt => opt.MapFrom(src => src.Birthday))
             .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.TelephoneNumber))
             .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.Image))
